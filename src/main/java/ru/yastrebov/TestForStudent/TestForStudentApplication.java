6dab1ea60e87e29e66112a47c45b1d849dae72dc
Ястребов Андрей Владимирович (4076463) <ayastrebov@dev.vtb.ru>
2022-12-11 18:13:29 +0300

    Initial project

---
+++ src/main/java/ru/yastrebov/TestForStudent/TestForStudentApplication.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.TestForStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForStudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yastrebov/TestForStudent/TestForStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.TestForStudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
